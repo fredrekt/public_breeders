dfc2acb225ade805d8f4f6a91ac7f5fd307db65a
fredrekt <fredrickjohng6@gmail.com>
2023-07-30 20:37:10 +0800

    updated designs

--- src/pages/privacyPolicy/PrivacyPolicypage.tsx
+++ src/pages/privacyPolicy/PrivacyPolicypage.tsx
@@ -3,14 +3,14 @@ import './PrivacyPolicypage.scss';
 import axios from 'axios';
 import { API_URL } from '../../utils/constant';
 import { message } from 'antd';
-import { getToken } from '../../utils/authHelpers';
-import ProtectedRoute from '../../utils/ProtectedRoute';
-import PrivateLayout from '../../layouts/private/PrivateLayout';
-import PublicLayout from '../../layouts/public/PublicLayout';
+// import { getToken } from '../../utils/authHelpers';
+// import ProtectedRoute from '../../utils/ProtectedRoute';
+// import PrivateLayout from '../../layouts/private/PrivateLayout';
+// import PublicLayout from '../../layouts/public/PublicLayout';
 import ReactMarkdown from 'react-markdown';
 
 const PrivacyPolicypage: React.FC = () => {
-	const loggedIn: boolean = getToken() !== '';
+	// const loggedIn: boolean = getToken() !== '';
 	const [content, setContent] = useState<any>();
 
 	const loadTerms = async () => {
@@ -25,26 +25,31 @@ const PrivacyPolicypage: React.FC = () => {
 	useEffect(() => {
 		loadTerms();
 	}, []);
-    
-    const renderContent = () => {
-		if (loggedIn) {
-			return (
-				<ProtectedRoute>
-					<PrivateLayout className="privateTermsAndConditionsPage">
-						<ReactMarkdown className='richtextContent'>{content}</ReactMarkdown>
-					</PrivateLayout>
-				</ProtectedRoute>
-			);
-		} else {
-			return (
-				<PublicLayout navbar className="publicTermsAndConditionsPage">
-					<ReactMarkdown className='richtextContent'>{content}</ReactMarkdown>
-				</PublicLayout>
-			);
-		}
-	};
 
-	return renderContent();
+	// const renderContent = () => {
+	// 	if (loggedIn) {
+	// 		return (
+	// 			<ProtectedRoute>
+	// 				<PrivateLayout className="privateTermsAndConditionsPage">
+	// 					<ReactMarkdown className="richtextContent">{content}</ReactMarkdown>
+	// 				</PrivateLayout>
+	// 			</ProtectedRoute>
+	// 		);
+	// 	} else {
+	// 		return (
+	// 			<PublicLayout navbar className="publicTermsAndConditionsPage">
+	// 				<ReactMarkdown className="richtextContent">{content}</ReactMarkdown>
+	// 			</PublicLayout>
+	// 		);
+	// 	}
+	// };
+
+	// return renderContent();
+	return (
+		<div className="publicTermsAndConditionsPage">
+			<ReactMarkdown className="richtextContent">{content}</ReactMarkdown>
+		</div>
+	);
 };
 
 export default PrivacyPolicypage;
