7520b3ad496cec8e8898218d68e219d696dc8c16
fredrekt <fredrickjohng6@gmail.com>
2023-08-28 19:45:49 +0800

    coming soon: revert when ready to launch prod again

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,29 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import './App.scss';
-import Homepage from './pages/home/Homepage';
-import Loginpage from './pages/login/Loginpage';
-import Registerpage from './pages/register/Registerpage';
-import ForgotPassword from './pages/forgotPassword/ForgotPassword';
-import Breederpage from './pages/breeder/Breederpage';
-import Inboxpage from './pages/inbox/Inboxpage';
-import Favoritespage from './pages/favorites/Favoritespage';
-import Orderspage from './pages/orders/Orderspage';
-import Profilepage from './pages/profile/Profilepage';
-import Animalpage from './pages/animal/Animalpage';
-import ProtectedRoute from './utils/ProtectedRoute';
-import Dashboardpage from './pages/dashboard/Dashboardpage';
-import Aboutpage from './pages/about/Aboutpage';
-import Contactpage from './pages/contact/Contactpage';
-import Demopage from './pages/demo/Demopage';
-import AppStorepage from './pages/appStore/AppStorepage';
+// import Homepage from './pages/home/Homepage';
+// import Loginpage from './pages/login/Loginpage';
+// import Registerpage from './pages/register/Registerpage';
+// import ForgotPassword from './pages/forgotPassword/ForgotPassword';
+// import Breederpage from './pages/breeder/Breederpage';
+// import Inboxpage from './pages/inbox/Inboxpage';
+// import Favoritespage from './pages/favorites/Favoritespage';
+// import Orderspage from './pages/orders/Orderspage';
+// import Profilepage from './pages/profile/Profilepage';
+// import Animalpage from './pages/animal/Animalpage';
+// import ProtectedRoute from './utils/ProtectedRoute';
+// import Dashboardpage from './pages/dashboard/Dashboardpage';
+// import Aboutpage from './pages/about/Aboutpage';
+// import Contactpage from './pages/contact/Contactpage';
+// import Demopage from './pages/demo/Demopage';
+// import AppStorepage from './pages/appStore/AppStorepage';
 import { message } from 'antd';
 import TermsAndConditionsPage from './pages/termsAndConditions/TermsAndConditionsPage';
 import PrivacyPolicypage from './pages/privacyPolicy/PrivacyPolicypage';
-import BreederOnboardingPayoutpage from './pages/breederOnboardingPayout/BreederOnboardingPayoutpage';
-import SuccessPage from './pages/stripe/SuccessPage';
+// import BreederOnboardingPayoutpage from './pages/breederOnboardingPayout/BreederOnboardingPayoutpage';
+// import SuccessPage from './pages/stripe/SuccessPage';
 import MissingPage from './pages/missing/MissingPage';
+import ComingSoonpage from './pages/comingSoon/ComingSoonpage';
 
 message.config({
 	duration: 1,
@@ -33,7 +34,8 @@ const App: React.FC = () => {
 	return (
 		<>
 			<Routes>
-				<Route path="/" index element={<Homepage />} />
+				<Route path="/" index element={<ComingSoonpage/>} />
+				{/* <Route path="/" index element={<Homepage />} />
 				<Route path="/login" element={<Loginpage />} />
 				<Route path="/register" element={<Registerpage />} />
 				<Route path="/forgot-password" element={<ForgotPassword />} />
@@ -41,11 +43,11 @@ const App: React.FC = () => {
 				<Route path="/about" element={<Aboutpage />} />
 				<Route path="/contact" element={<Contactpage />} />
 				<Route path="/demo" element={<Demopage />} />
-				<Route path="/mobile" element={<AppStorepage />} />
+				<Route path="/mobile" element={<AppStorepage />} /> */}
 				<Route path="/terms-and-conditions" element={<TermsAndConditionsPage/>} />
 				<Route path="/privacy-policy" element={<PrivacyPolicypage/>} />
-				<Route path="/stripe/success" element={<SuccessPage />} />
-				<Route
+				{/* <Route path="/stripe/success" element={<SuccessPage />} /> */}
+				{/* <Route
 					path="/breeder/:id"
 					element={
 						<ProtectedRoute>
@@ -108,7 +110,7 @@ const App: React.FC = () => {
 							<BreederOnboardingPayoutpage />
 						</ProtectedRoute>
 					}
-				/>
+				/> */}
 				<Route path="*" element={<MissingPage />} />
 			</Routes>
 		</>
